c737d42af57dd90f496c3b547c42567edea5a029
sahilister <sahil.dhiman27091999@gmail.com>
2020-06-03 17:48:02 +0530

    add create react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <section className="navbar is-light">
+          <div className="navbar-body">
+            <div className="container">
+              <h1 className="title">Profile Search</h1>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
